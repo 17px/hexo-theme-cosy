23f583578811cbd50de88468e75c221e5146b88f
mozzie <himozzie@foxmail.com>
2023-11-11 19:23:32 +0800

    fix: article top field sorting bug

--- apps/dockyard/src/extend/sort_posts.js
+++ apps/dockyard/src/extend/sort_posts.js
@@ -1,14 +1,16 @@
 hexo.extend.helper.register('sort_posts_by_top', function (posts) {
-  // 使用 slice() 创建数组的副本，以避免修改原始数组
-  return posts.slice().sort(function (a, b) {
-    var topA = a.top || Number.MAX_SAFE_INTEGER;  // 如果没有 top 字段，则设置为一个非常大的数
-    var topB = b.top || Number.MAX_SAFE_INTEGER;  // 同上
 
-    // 根据 top 字段从小到大排序
+  return posts.slice().sort(function (a, b) {
+    var topA = a.top !== undefined ? a.top : Number.MAX_SAFE_INTEGER;
+    var topB = b.top !== undefined ? b.top : Number.MAX_SAFE_INTEGER;
+
+    // 根据 top 字段从小到大降序排列
     if (topA < topB) return -1;
     if (topA > topB) return 1;
 
-    // 如果 top 字段相同或者不存在，根据日期从新到旧排序
-    return b.date - a.date;
+    // 如果 top 字段相同或不存在，则根据日期从新到旧排序
+    var dateA = new Date(a.date);
+    var dateB = new Date(b.date);
+    return dateB - dateA;
   });
 });
